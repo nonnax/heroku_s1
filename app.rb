fe5ce19327e6a1c6c6b2768f152fda0f07552198
nonnax <ironald@gmail.com>
2022-03-30 00:46:55 +0800

    fix merge junk

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2022-03-01 15:24:22 +0800
-<<<<<<< HEAD
 require_relative 'lib/router'
 
 class App<Router; end
@@ -11,10 +10,3 @@ get  '/tv',    :tv
 post '/tvs',   :tvs 
 
 p routes
-=======
-require_relative 'lib/mrouter'
-
-get('/'){ :index }
-get('/movie'){ :movie }
-get('/tv'){ :tv }
->>>>>>> e26d7a745ba4ceb52511e73ebd9a88610d73b501
--- lib/view.rb
+++ lib/view.rb
@@ -4,24 +4,9 @@ class View
   attr :data
   def initialize(page, **data)
     @data = data
-<<<<<<< HEAD
     @template, @layout = [page, :layout].map do |f|
       File.expand_path("../public/views/#{f}.erb", __dir__ ).then{ |f| IO.read(f) }
     end
-=======
-    @template, @layout = [page, :layout].map do |f|      
-      File.expand_path("../public/views/#{f}.erb", __dir__ ).then{|f| File.read(f) }
-    end
-  end
-  def _render(text)
-    ERB.new(text).result(binding)
-  end
-  def render
-    [@template, @layout].inject{|text, t | _render(t){text}}
-  end
-  def visit_count
-    @data[:visit_count]
->>>>>>> e26d7a745ba4ceb52511e73ebd9a88610d73b501
   end
   def render() [@template, @layout].reduce{ |text, t| _render(t){text} } end
   def _render(text) ERB.new(text).result(binding) end
