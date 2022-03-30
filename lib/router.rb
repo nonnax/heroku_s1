33f96548e407821c64ab1e3c5d36101e97868b7e
nonnax <ironald@gmail.com>
2022-03-30 21:02:19 +0800

    cleanup

--- lib/router.rb
+++ lib/router.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2022-03-01 15:28:55 +0800
+# path to erb route mapper
 require_relative 'view'
 
 module Routes
@@ -13,14 +14,21 @@ class Router
   def call(env)
     @req=Rack::Request.new(env)
     @res=Rack::Response.new
-    headers={'Content-type'=>'text/html; charset=utf-8'}
-    route = ::Routes.detect(env)
-    puts route, visit_count: parse_cookies(env) if route
-    @res.status = 404 if @res.empty?
-    @res.finish
+    # headers={'Content-type'=>'text/html; charset=utf-8'}
+    match = ::Routes.detect(env)
+    puts match, visit_count: parse_cookies(env) if match
+    return res.finish unless res.empty?
+    not_found 
   end
   def parse_cookies(env) Rack::Utils.parse_cookies(env)["session_count"]  end
   def puts(route, **data) res.write( ::View.render(route[:erb], **data) ) end
+  
+  def not_found
+    res.status=404
+    res.write 'Not Found'
+    res.finish
+  end
+  
 end
 
 #global access
